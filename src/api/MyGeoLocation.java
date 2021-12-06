8c945d7bfbc8a21d11b55809889ce7884d384e1c
michaelMatve <michael.matve@gmail.com>
2021-12-06 14:49:23 +0200

    with imp

--- src/api/MyGeoLocation.java
+++ src/api/MyGeoLocation.java
@@ -1,4 +1,34 @@
 package api;
 
-public class MyGeoLocation {
+public class MyGeoLocation implements GeoLocation{
+
+    private double x;
+    private double y;
+    private double z;
+
+    public MyGeoLocation(double Nx, double Ny, double Nz)
+    {
+        this.x=Nx;
+        this.y=Ny;
+        this.z=Nz;
+    }
+    @Override
+    public double x() {
+        return this.x;
+    }
+
+    @Override
+    public double y() {
+        return this.y;
+    }
+
+    @Override
+    public double z() {
+        return this.z;
+    }
+
+    @Override
+    public double distance(GeoLocation g) {
+        return 0;
+    }
 }
