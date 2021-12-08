cd82a730e5f678adad70547bd6de1c97432a4f36
michaelMatve <michael.matve@gmail.com>
2021-12-09 01:27:36 +0200

    firest load save finish

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import api.MyGsonGraph;
 public class Main {
     public static void main(String[] args) {
         MyDirectedWeightedGraphAlgorithms algo = new MyDirectedWeightedGraphAlgorithms();
-        algo.load("C:\\Users\\Dell\\IdeaProjects\\OOP_Ex2\\src\\api\\G1.json");
+        algo.load("C:\\Users\\Dell\\IdeaProjects\\OOP_Ex2\\data\\G3.json");
         System.out.println(algo.getGraph().edgeSize());
         System.out.println(algo.getGraph().nodeSize());
         algo.save("michael.json");
