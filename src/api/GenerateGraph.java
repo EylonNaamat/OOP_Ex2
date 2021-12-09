7d7716cbf1af340e9fddda8ca58f9f76535468fa
EylonNaamat <eylonnaamat@gmail.com>
2021-12-09 17:22:33 +0200

    Ex2 and generate graph

---
+++ src/api/GenerateGraph.java
@@ -0,0 +1,36 @@
+package api;
+
+import java.util.Random;
+
+public class GenerateGraph {
+    private int nodesNum;
+    private int edgesNum;
+
+
+    public GenerateGraph(int nodesNum, int edgesNum){
+        this.nodesNum = nodesNum;
+        this.edgesNum = edgesNum;
+    }
+
+    public DirectedWeightedGraph generate(){
+        MyDirectedWeightedGraph ans = new MyDirectedWeightedGraph();
+        Random r = new Random();
+        for (int i = 0; i < this.nodesNum; i++) {
+            double randX = 35 + r.nextDouble();
+            double randy = 32 + r.nextDouble();
+            MyGeoLocation loc = new MyGeoLocation(randX, randy, 0.0);
+            MyNodeData node = new MyNodeData(i, loc);
+            ans.addNode(node);
+        }
+
+        for (int i = 0; i < this.nodesNum; i++) {
+            for (int j = 0; j < this.edgesNum; j++) {
+                int randDest = r.nextInt(this.nodesNum);
+                double randWeight = 1 + r.nextDouble();
+                ans.connect(i, randDest, randWeight);
+            }
+        }
+        return ans;
+    }
+
+}
