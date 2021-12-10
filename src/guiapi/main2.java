38ecfa5f4240f5b67b8b1921e2e9454bb4d81649
EylonNaamat <eylonnaamat@gmail.com>
2021-12-10 17:52:27 +0200

    updated Gui_Menu

--- src/guiapi/main2.java
+++ src/guiapi/main2.java
@@ -7,7 +7,7 @@ public class main2 {
 
     public static void main(String[] args) {
         MyDirectedWeightedGraphAlgorithms algo = new MyDirectedWeightedGraphAlgorithms();
-        algo.load("C:\\Users\\Dell\\IdeaProjects\\OOP_Ex2\\src\\api\\G1.json");
+        algo.load("C:\\Users\\eylon\\IdeaProjects\\OOP_Ex2\\data\\G1.json");
         new Gui_MyGraphDrow(algo);
     }
 }
